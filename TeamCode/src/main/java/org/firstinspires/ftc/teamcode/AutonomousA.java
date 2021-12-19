86625cef3d8c1a6fd293a40fdcb20930c72c8025
sontheteacher <sontheteacher0605@gmail.com>
2021-12-19 08:08:58 -0500

    Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousA.java
@@ -87,7 +87,7 @@ public class AutonomousA extends LinearOpMode {
         // Step through each leg of the path, ensuring that the Auto mode has not been stopped along the way
 
 
-        // Step 1:  Move right a bit and drive Forwards for 5 Seconds
+        // Step 1: Strafe right a bit
         robot.move(0, STRAFE_SPEED, 0);
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < 0.5)) {
@@ -95,11 +95,11 @@ public class AutonomousA extends LinearOpMode {
             telemetry.update();
         }
 
-        //STEP 2: Drive Forwards for 4.5 Seconds
+        //STEP 2: Drive Forwards for 3.5 Seconds and stop
         robot.move(FORWARD_SPEED, 0, 0);
         runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 4.5)) {
-            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+        while (opModeIsActive() && (runtime.seconds() < 3.5)) {
+            telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.seconds());
             telemetry.update();
         }
         robot.move(0,0,0);
@@ -107,19 +107,36 @@ public class AutonomousA extends LinearOpMode {
         // Step 2: Move the carousel
         robot.carousel(true);
         runtime.reset();
-        while(opModeIsActive() && (runtime.seconds() < 3.0)) {
-            telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.seconds());
+        while(opModeIsActive() && (runtime.seconds() < 4.0)) {
+            telemetry.addData("Path", "Leg 3: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        robot.carousel(false);
+
+        // Step 3:  Drive Backward for 3.5 Seconds
+        robot.move(-FORWARD_SPEED, 0, 0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 3.5)) {
+            telemetry.addData("Path", "Leg 4: %2.5f S Elapsed", runtime.seconds());
             telemetry.update();
         }
 
-        // Step 3:  Drive Backward for 10 Seconds
-        robot.move(-FORWARD_SPEED, 0, 0);
+        // Step 4: Strafe left a bit
+        robot.move(0, -STRAFE_SPEED, 0);
         runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 8.0)) {
-            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+        while (opModeIsActive() && (runtime.seconds() < 0.4)) {
+            telemetry.addData("Path", "Leg 5: %2.5f S Elapsed", runtime.seconds());
             telemetry.update();
         }
         robot.move(0,0,0);
+
+        // Step 5: Drive Backward for 1.5 Seconds to the warehouse
+        robot.move(-FORWARD_SPEED, 0, 0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.5)) {
+            telemetry.addData("Path", "Leg 6: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
 
 
         telemetry.addData("Path", "Complete");
